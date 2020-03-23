dc1710662e87600276f843507e4aee0757dce229
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-03-23 15:54:38 -0600

    added class names to html elements, and added colors as variables in css

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ form.addEventListener('submit', e => {
     }
 });
 
+// email validation regex code from stack overflow
 function isValidEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
